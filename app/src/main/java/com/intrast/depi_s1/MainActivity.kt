d9360d9703fbe377bebd3be7ecca23d79effcd0f
Ahmed Fathy <Ahmed.fathy9707@gmail.com>
2024-12-20 16:00:52 +0200

    added login feature

--- app/src/main/java/com/intrast/depi_s1/MainActivity.kt
+++ app/src/main/java/com/intrast/depi_s1/MainActivity.kt
@@ -10,41 +10,10 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    lateinit var textView:TextView    // declare "Global variable"
-    lateinit var leftButton: Button
-    lateinit var rigthButton:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        textView = findViewById(R.id.textView2)
-        leftButton = findViewById(R.id.left_button)
-        rigthButton = findViewById(R.id.right_button)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(R.layout.login_screen)
 
-
-        leftButton.setOnClickListener {
-            leftButtonClicked()
-        }
-        rigthButton.setOnClickListener{
-            rightButtonClicked()
-        }
     }
-
-    fun rightButtonClicked(){
-        textView.text = "Right Button Clicked"
-        rigthButton.text = "clicked"
-        leftButton.text = "Left"
-    }
-
-    fun leftButtonClicked(){
-        textView.text = "Left Button Clicked"
-        leftButton.text = "clicked"
-        rigthButton.text = "Right"
-    }
-
 }
