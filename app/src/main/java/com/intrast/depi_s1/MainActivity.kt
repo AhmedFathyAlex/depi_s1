befc716658a6128bed23d9daa8c696c5a1127fe2
Ahmed Fathy <Ahmed.fathy9707@gmail.com>
2024-12-20 16:05:41 +0200

    add some comment

--- app/src/main/java/com/intrast/depi_s1/MainActivity.kt
+++ app/src/main/java/com/intrast/depi_s1/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        // add some comment
         setContentView(R.layout.login_screen)
 
     }
